35f2f9cc79f31a4218bb9b022f622a3ba93fe5f5
Łukasz Gumiński <lukasz.guminski629@gmail.com>
2024-12-26 19:29:32 +0100

    Changes done

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -4,19 +4,13 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
-import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
 
 @LambdaHandler(
     lambdaName = "hello_world",
@@ -29,27 +23,35 @@ import java.util.function.Function;
     authType = AuthType.NONE,
     invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, Map<String, Object>> {
+public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
   private final Map<String, String> responseHeaders = Map.of("Content-Type", "application/json");
 
-  public Map<String, Object> handleRequest(APIGatewayV2HTTPEvent requestEvent,
+  public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent,
       Context context) {
     var method = requestEvent.getRequestContext().getHttp().getMethod();
     var path = requestEvent.getRequestContext().getHttp().getPath();
     if (method.equals("GET") && path.equals("/hello")) {
-      var response = new HashMap<String, Object>();
-      response.put("statusCode", 200);
-      response.put("message", "Hello from Lambda");
-      return response;
+      return APIGatewayV2HTTPResponse.builder()
+          .withStatusCode(200)
+          .withHeaders(responseHeaders)
+          .withBody(String.format("{\"statusCode\": %d, \"message\": \"%s\"}", 200, "Hello from Lambda"))
+          .build();
     } else {
-      var response = new HashMap<String, Object>();
-      response.put("statusCode", 400);
-      response.put("message", String.format(
-          "Bad request syntax or unsupported method. Request path: %s. HTTP method: %s",
-          path, method
-      ));
-      return response;
+      return APIGatewayV2HTTPResponse.builder()
+          .withStatusCode(400)
+          .withHeaders(responseHeaders)
+          .withBody(
+              String.format(
+                  "{\"statusCode\": %d, \"message\": \"%s\"}",
+                  400,
+                  String.format(
+                      "Bad request syntax or unsupported method. Request path: %s. HTTP method: %s",
+                      path, method)
+              )
+
+          )
+          .build();
     }
   }
 
